f83eb9f257a757202814cab6f006cb678daf909d
josealonso <jose.alonso.programmer@gmail.com>
2021-07-19 17:31:53 +0200

    Using Events is the solution.

--- EventExample.sol
+++ EventExample.sol
@@ -3,10 +3,15 @@ pragma solidity ^0.5.11;
 
 // If the smart contract is deployed in the Javascript VM, it works.
 // But it doesn't work on real blockchains ("Injected Web3" option on Remix), since the output is empty.
+// Return values are meant for inter-smart-contract communication, they won’t return anything to the
+// transaction origin. 
+// SOLUTION ---> using Events for outputting anything in writing transactions.
 
 contract EventExample {
     
     mapping(address => uint) public tokenBalance;
+
+    event TokensSent(address _from, address _to, uint _amount);
 
     constructor() public {
         tokenBalance[msg.sender] = 100;
@@ -18,6 +23,8 @@ contract EventExample {
         assert(tokenBalance[msg.sender] - _amount <= tokenBalance[msg.sender]);
         tokenBalance[msg.sender] -= _amount;
         tokenBalance[_to] += _amount;
+
+        emit TokensSent(msg.sender, _to, _amount);
 
         return true;
     }
