f06f0b691dae0246f85f8d9ae4d46182f9bc2bed
josealonso <jose.alonso.programmer@gmail.com>
2021-07-22 15:47:56 +0200

    Added an event to notify a change in allowance.

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -4,55 +4,46 @@ pragma solidity >=0.7.0 <0.9.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.2.0/contracts/access/Ownable.sol";
 
 contract Allowance is Ownable {
-    
-    mapping(address => uint) public allowance;
+    mapping(address => uint256) public allowance;
 
-    function addAllowance(address _who, uint _amount) public onlyOwner {
+    function addAllowance(address _who, uint256 _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
         allowance[_who] = _amount;
-    }    
+    }
 
-    modifier ownerOrAllowed(uint _amount) {
-        require(owner() == msg.sender || allowance[msg.sender] >= _amount, "You are not allowed.");
+    modifier ownerOrAllowed(uint256 _amount) {
+        require(
+            owner() == msg.sender || allowance[msg.sender] >= _amount,
+            "You are not allowed."
+        );
         _;
     }
 
-    function reduceAllowance(address _who, uint _amount) internal {
-        allowance[_who] = allowance[_who] - _amount; 
+    function reduceAllowance(address _who, uint256 _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
+        allowance[_who] = allowance[_who] - _amount;
     }
-    
 }
 
-// Thi is the contract that must be deployed
+// This is the contract that must be deployed
 contract SimpleWallet is Allowance {
-
-/*
-    mapping(address => uint) public allowance;
-
-    function addAllowance(address _who, uint _amount) public onlyOwner {
-        allowance[_who] = _amount;
-    }    
     
-    modifier ownerOrAllowed(uint _amount) {
-        require(owner() == msg.sender || allowance[msg.sender] >= _amount, "You are not allowed.");
-        _;
-    }
-    
-    function reduceAllowance(address _who, uint _amount) internal {
-        // require(_amount <= allowance[_who], "");
-        allowance[_who] = allowance[_who] - _amount; 
-    }    */
-    
-    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {  
-        require(_amount <= address(this).balance, "There are not enough funds stored in the smart contact");         
+    function withdrawMoney(address payable _to, uint256 _amount)
+        public
+        ownerOrAllowed(_amount)
+    {
+        require(
+            _amount <= address(this).balance,
+            "There are not enough funds stored in the smart contact"
+        );
         // Structure to avoid double spending
         if (owner() != msg.sender) {
             reduceAllowance(_to, _amount);
         }
         _to.transfer(_amount);
-        
     }
-    
-    receive() external payable {    // deposit money into the SC
-        
+
+    receive() external payable {
+        // deposit money into the SC
     }
-}    
+}
