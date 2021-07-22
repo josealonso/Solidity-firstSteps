09e1637b5d85b220614bba18b0840db709bca913
josealonso <jose.alonso.programmer@gmail.com>
2021-07-22 16:05:34 +0200

    Added events for receiving and sending money.

--- SimpleWallet.sol
+++ SimpleWallet.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.7.0 <0.9.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.2.0/contracts/access/Ownable.sol";
 
 contract Allowance is Ownable {
+
     mapping(address => uint256) public allowance;
 
     function addAllowance(address _who, uint256 _amount) public onlyOwner {
@@ -27,6 +28,9 @@ contract Allowance is Ownable {
 
 // This is the contract that must be deployed
 contract SimpleWallet is Allowance {
+
+    event MoneySent(address indexed _beneficiary, uint _amount);
+    event MoneyReceived(address indexed _from, uint _amount);
     
     function withdrawMoney(address payable _to, uint256 _amount)
         public
@@ -40,10 +44,11 @@ contract SimpleWallet is Allowance {
         if (owner() != msg.sender) {
             reduceAllowance(_to, _amount);
         }
+        emit MoneySent(_to, _amount);
         _to.transfer(_amount);
     }
 
-    receive() external payable {
-        // deposit money into the SC
+    receive() external payable {  // deposit money into the SC
+        emit MoneyReceived(msg.sender, msg.value);
     }
 }
