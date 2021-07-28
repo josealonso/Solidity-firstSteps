55f0cf722a1f434113641c6b4a7241b7e0d113b7
josealonso <jose.alonso.programmer@gmail.com>
2021-07-28 18:15:45 +0200

    Deleted a file by which it couldn't be deployed.

--- AssetsTokenization-Project/contracts/MyToken.sol
+++ AssetsTokenization-Project/contracts/MyToken.sol
@@ -3,8 +3,6 @@ pragma solidity >=0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// Added ERC20 OpenZeppelin contract, version 4.x
-
 contract MyToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Complutum Token", "CPL") {
         _mint(msg.sender, initialSupply);
