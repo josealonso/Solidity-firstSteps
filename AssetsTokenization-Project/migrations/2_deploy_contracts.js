5c75e5446fa6afac116b1e0115a8af2a0b8be2cd
josealonso <jose.alonso.programmer@gmail.com>
2021-07-30 17:35:58 +0200

    Added the migration for the MyTokenSale smart contract. Ganache had to be started.

--- AssetsTokenization-Project/migrations/2_deploy_contracts.js
+++ AssetsTokenization-Project/migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 var MyToken = artifacts.require("./MyToken.sol");
+var MyTokenSale = artifacts.require("./MyTokenSale");
 
 module.exports = async function(deployer) {
+  let addr = await web3.eth.getAccounts();
   await deployer.deploy(MyToken, 10000);
+  // transfer all the tokens from the msg sender to the MyTokenSales smart contract 
+  await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
+  let instance = await MyToken.deployed();
+  await instance.transfer(MyTokenSale.address, 10000);
 };
