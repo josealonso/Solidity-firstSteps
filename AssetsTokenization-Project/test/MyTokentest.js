453ee56e345e1753c7aa9c1e16d0c1ae2c1d1ff8
josealonso <jose.alonso.programmer@gmail.com>
2021-07-30 19:51:36 +0200

    Fixed the unit test by using the "beforeEach" hook.

--- AssetsTokenization-Project/test/MyTokentest.js
+++ AssetsTokenization-Project/test/MyTokentest.js
@@ -15,8 +15,13 @@ const expect = chai.expect;
 contract("Token Test", async accounts => {
     const [initialHolder, recipient, anotherAccount] = accounts;
 
+    beforeEach(async () => {
+        this.myToken = await Token.new(1000);
+    });
+
     it("all tokens should be in my account", async () => {
-        let instance = await Token.deployed();
+        // let instance = await Token.deployed();
+        let instance = this.myToken;
         let totalSupply = await instance.totalSupply();
         // expect(await instance.balanceOf(accounts[0])).to.be.a.bignumber.equal(totalSupply);
         expect(instance.balanceOf(accounts[0])).eventually.be.a.bignumber.equal(totalSupply);
@@ -24,7 +29,8 @@ contract("Token Test", async accounts => {
 
     it("I can send tokens from Account 1 to Account 2", async () => {
         const sendTokens = 1;
-        let instance = await Token.deployed();
+        let instance = this.myToken;
+        // let instance = await Token.deployed();
         let totalSupply = await instance.totalSupply();
         await expect(instance.balanceOf(initialHolder)).to.eventually.be.a.bignumber.equal(totalSupply);
         await expect(instance.transfer(recipient, sendTokens)).to.eventually.be.fulfilled;
@@ -33,7 +39,8 @@ contract("Token Test", async accounts => {
     });
 
     it("It's not possible to send more tokens than account 1 has", async () => {
-        let instance = await Token.deployed();
+        let instance = this.myToken;
+        // let instance = await Token.deployed();
         let balanceOfAccount = await instance.balanceOf(initialHolder);
         await expect(instance.transfer(recipient, new BN(balanceOfAccount + 1))).to.eventually.be.rejected;
 
