fef618c28891e59742da16426fc5665b56434481
josealonso <jose.alonso.programmer@gmail.com>
2021-07-30 16:31:33 +0200

    Modified the Crowdsale Contract from Open-Zeppelin 2.5 to be available for Solidity 0.6. Written own Crowdsale on top of it.

--- AssetsTokenization-Project/contracts/MyToken.sol
+++ AssetsTokenization-Project/contracts/MyToken.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
+pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("Complutum Token", "CPL") {
+    constructor(uint256 initialSupply) public ERC20("Complutum Token", "CPL") {
         _mint(msg.sender, initialSupply);
     }
 
--- AssetsTokenization-Project/truffle-config.js
+++ AssetsTokenization-Project/truffle-config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   compilers: {
     solc: {
-      version: "0.8.4"
+      version: "0.6.0"
     }
   }
 };
