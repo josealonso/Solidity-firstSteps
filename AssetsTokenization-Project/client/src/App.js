898430374612e55ed02277d72e17e3cac0003507
josealonso <jose.alonso.programmer@gmail.com>
2021-08-03 21:02:29 +0200

    Added the KYC Whitelisting functionality to the frontend.

--- AssetsTokenization-Project/client/src/App.js
+++ AssetsTokenization-Project/client/src/App.js
@@ -7,7 +7,7 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 
 class App extends Component {
-  state = { loaded: false };
+  state = { loaded: false, kycAddress: "0x123" };
 
   componentDidMount = async () => {
     try {
@@ -48,23 +48,31 @@ class App extends Component {
     }
   };
 
+  handleInputChange = (event) => {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleKycWhitelisting = async () => {
+    await this.kycInstance.setKycCompleted(this.state.kycAddress).send({ from: this.accounts[0] });
+    alert("KYC for " + this.state.kycAddress + " is completed");
+  }
+
   render() {
     if (!this.state.loaded) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Complu Token Sale</h1>
+        <p>Get your tokens today!</p>
+        <h2>kyc Whitelisting</h2>
+        Address to allow: <input type="text" name="kycAddress" value={this.state.kycAddress} onChange={this.handleInputChange} />
+        <button type="button" onClick={this.handleKycWhitelisting}>Add to Whitelist</button>
       </div>
     );
   }
