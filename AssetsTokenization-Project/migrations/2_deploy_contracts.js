b657605e3892e89f408be8c1abfde7212491093b
josealonso <jose.alonso.programmer@gmail.com>
2021-08-03 18:46:02 +0200

    Added a KYC Smart Contract Solution Mockup.

--- AssetsTokenization-Project/migrations/2_deploy_contracts.js
+++ AssetsTokenization-Project/migrations/2_deploy_contracts.js
@@ -1,13 +1,14 @@
 var MyToken = artifacts.require("./MyToken.sol");
 var MyTokenSale = artifacts.require("./MyTokenSale");
-require('dotenv').config({path: '../.env'});
+var MyKycContract = artifacts.require("./KycContract");
 
+require('dotenv').config({path: '../.env'});
 
 module.exports = async function(deployer) {
   let addr = await web3.eth.getAccounts();
   await deployer.deploy(MyToken, process.env.INITIAL_TOKENS);
-  // transfer all the tokens from the msg sender to the MyTokenSales smart contract 
-  await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
+  await deployer.deploy(MyKycContract);
+  await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address, MyKycContract.address);
   let instance = await MyToken.deployed();
   await instance.transfer(MyTokenSale.address, process.env.INITIAL_TOKENS);
 };
--- AssetsTokenization-Project/test/MyTokenSale.test.js
+++ AssetsTokenization-Project/test/MyTokenSale.test.js
@@ -1,5 +1,6 @@
 const TokenSale = artifacts.require("MyTokenSale");
 const Token = artifacts.require("MyToken");
+const KycContract = artifacts.require("KycContract");
 
 const chai = require("./setupchai.js");
 const BN = web3.utils.BN;
@@ -27,6 +28,8 @@ contract("TokenSale Test", async accounts => {
     it("should be possible to buy one token by simply sending ether to the smart contract", async () => {
         let tokenInstance = await Token.deployed();
         let tokenSaleInstance = await TokenSale.deployed();
+        let kycInstance = await KycContract.deployed();
+        await kycInstance.setKycCompleted(recipient);
         let balanceBeforeAccount = await tokenInstance.balanceOf.call(recipient);
         await expect(tokenSaleInstance.sendTransaction({from: recipient, value: web3.utils.toWei("1", "wei")}) ).to.be.fulfilled;
         return expect(balanceBeforeAccount + 1).to.be.a.bignumber.equal(await tokenInstance.balanceOf.call(recipient));
